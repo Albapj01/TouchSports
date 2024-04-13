85587ba11a5a7f09923d7ee1839c5fc7e7979e9c
Albapj01 <83663505+Albapj01@users.noreply.github.com>
2024-04-13 14:44:29 +0200

    feat(backend): :construction: created trainerPort and reserve class

---
+++ backend/src/trainers/domain/model/Reserve.ts
@@ -0,0 +1,10 @@
+export class Reserve {
+    constructor(
+        readonly name: string,
+        readonly surname: string,
+        readonly email: string,
+        readonly telephone: string,
+        readonly meterial: string,
+        readonly date: Date,
+    ) {}
+  }
